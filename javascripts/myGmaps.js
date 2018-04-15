fbcc572d542d406ac18a375ff27653a98d793762
mwilson1 <ml_wilson1@yahoo.com>
2018-04-15 08:16:52 -0400

    better message and dialog boxes

--- javascripts/myGmaps.js
+++ javascripts/myGmaps.js
@@ -31,50 +31,83 @@ let today = new Date().toISOString().split('T')[0]; // get only yyyy-mm-dd
 let datechoose = document.getElementById("parkdate");
 datechoose.min = today;
 
-
+ 
 function messagebox(text){
     // add text and ok button
     // display message box
     // query for the ok button
     // add eventlistener to button
 
-    box = document.getElementsByClassName("msgbox")[0];
-    box.innerHTML = text+`<br><center><button class="close">OK</button></center>`;
-    box.style.display = "block";
-    okbutton = document.querySelector("button.close");
-    okbutton.addEventListener('click', function(){ box.style.display = "none" }  );
+    let boxcheck = document.getElementsByClassName("msgbox");
+    if (boxcheck.length == 0)
+    {
+        let bodytag = document.querySelector("body");
+        let box_node = document.createElement("div");
+        box_node.className = "msgbox" ; 
+        bodytag.appendChild( box_node );
+
+        let box = document.getElementsByClassName("msgbox")[0];
+
+        box.innerHTML = text+`<br><center><button class="close">OK</button></center>`;
+        box.style.display = "block";
+
+        okbutton = document.querySelector("button.close");
+        okbutton.addEventListener(
+            'click', function(){ 
+                box.style.display = "none" ;
+                bodytag.removeChild( box );
+            }  
+        );
+    }
 }
 
-function bookingbox(text){
-    // add text along with Back and Cancel button
-    // display message box
-    // query for the ok button
-    // add eventlistener to button
 
-    let box = document.getElementsByClassName("msgbox")[0];
-    box.innerHTML = text+`<br><div class="rowincolumn"> <button class="close">Back</button> <button id="cancel_booking">Cancel</button> </div>`;
-    box.style.display = "block";
-    let backbutton = document.querySelector("button.close");
-    backbutton.addEventListener('click', function(){ box.style.display = "none" }  );
-    let cancelbutton = document.querySelector("#cancel_booking");
+function bookingbox(text, ID){
+    function thebox(text, ID){               
+        let bodytag = document.querySelector("body");
+        let box_node = document.createElement("div");
+        box_node.id = "box_"+ID ; 
+        box_node.className = "msgbox" ; 
+        bodytag.appendChild( box_node );
 
-    cancelbutton.addEventListener('click', 
-    function(){ // open other window
-        let text2 = `<h3>Are you sure?</h3><p>There will be a fee if you cancel.</p>`;
-        bookingbox2(text2);  
+        let box = document.getElementById("box_"+ID);
+
+        box.innerHTML = text+`<br><div class="rowincolumn"> <button class="choice" id="back_`+ID+`">Back</button> <button class="choice" id="cancel_`+ID+`">Cancel</button> </div>`;
+        box.style.display = "block";
+
+        let backbutton = document.querySelector("#back_"+ID);
+        backbutton.addEventListener(
+            'click', function(){ 
+                box.style.display = "none";
+                bodytag.removeChild( box ); 
+            }  );
+        let cancelbutton = document.querySelector("#cancel_"+ID);
+
+        return cancelbutton
     }
-    );
+
+    if (ID == 1)
+    {
+        let boxcheck = document.getElementById("box_"+ID);
+        if (boxcheck == null)
+        {
+            let cancelbutton = thebox(text, ID);
+            cancelbutton.addEventListener(
+                'click', function(){ // open other window
+                let text2 = `<h3>Are you sure want to cancel?</h3><p>If you cancel, you'll be charged a $1.00 fee.</p>`;
+                let cb2 = thebox(text2, 2); 
+                // do nothing with the button for now.
+                }
+            );
+        }
+    }
+    else 
+    {
+        // do nothing as of now.
+        // go to next page, I think.
+    }
     
-};
-function bookingbox2(text){
-    let box = document.getElementsByClassName("msgbox2")[0];
-    box.innerHTML = text+`<br><div class="rowincolumn"> <button class="close2">Back</button> <button id="cancel_booking2">Cancel</button> </div>`;
-    box.style.display = "block";
-    let backbutton = document.querySelector("button.close2");
-    backbutton.addEventListener('click', function(){ box.style.display = "none" }  );
-    let cancelbutton = document.querySelector("#cancel_booking2");
-    //cancelbutton.addEventListener('click', function(){ box.style.display = "none" }  );
-};
+}
 
 
 function bookingtext(address, date, tstart, tfinal){
@@ -84,6 +117,7 @@ function bookingtext(address, date, tstart, tfinal){
 
 
 //======= Code that needs Google Maps API ===========
+
 
 function addMarker(location, tag){ // This adds and displays a marker.
     let marker = new google.maps.Marker( 
@@ -110,6 +144,7 @@ function initialMapcenter(userlatitude, userlongitude){ // When the user first r
 };
 
 function initMap() { // get or pick location to center on Map
+
     function geo_success(position){  // get user's location 
         userlatitude = position.coords.latitude;
         userlongitude = position.coords.longitude;
@@ -117,6 +152,7 @@ function initMap() { // get or pick location to center on Map
 
         initialMapcenter(userlatitude, userlongitude); 
     };
+
     function geo_error(){ // use phony location
         console.log('something went wrong.');
 
@@ -125,7 +161,7 @@ function initMap() { // get or pick location to center on Map
         localStorage.userposition = JSON.stringify(  {lat: userlatitude, lng: userlongitude}  );
 
         initialMapcenter(userlatitude, userlongitude); 
-    }
+    };
 
     if ("geolocation" in navigator)
     {
@@ -154,8 +190,12 @@ function initMap() { // get or pick location to center on Map
                 messagebox(mymessage);
             }
             else
-            {
-                geocodeAddress(geocoder, map);
+            {   
+                let boxcheck = document.getElementsByClassName("msgbox");
+                if (boxcheck.length == 0)
+                {
+                    geocodeAddress(geocoder, map);
+                }
             }
 
             }
@@ -256,9 +296,10 @@ function formFilled(pos){
         markers[i].addListener(
             'click', function(){ 
                 infowindows[this.tagger].open( map, this ) // the FOR loop is not active here so the "i" counter isn't available.
+
                 bookbuttons = document.getElementsByClassName("book");
                 bookbutton = bookbuttons[bookbuttons.length-1];
-                bookbutton.addEventListener( 'click', function(){ let textstuff = `<p>You booked it!</p><p>However, you can cancel this reservation.</p>`; bookingbox(textstuff, "msgbox") });
+                bookbutton.addEventListener( 'click', function(){ let textstuff = `<h3>You're all set!</h3><p>However, you can cancel this reservation.</p>`; bookingbox(textstuff, 1) });
 
             }
         );
